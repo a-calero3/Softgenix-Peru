6cb602b3c5bc6a24b6392ea6dd223e84c3f28140
Cordova-Rafael <rafaelcorsa97@gmail.com>
2023-05-25 17:55:43 -0500

    Administrar Productos implementado

--- 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Test.java
+++ 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Test.java
@@ -8,9 +8,11 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Cliente obj = new Cliente();
-        List<Cliente> lista = new LinkedList<Cliente>();
-        obj.runCliente(lista);
+       // Cliente obj = new Cliente();
+        Producto obj2 = new Producto();
+       // List<Cliente> lista = new LinkedList<Cliente>();
+        List<Producto> lista2 = new LinkedList<Producto>();
+        obj2.runTestProductos(lista2);
 
                                             }
 }
