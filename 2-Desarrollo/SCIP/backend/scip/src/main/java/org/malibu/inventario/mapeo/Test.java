ba1841aca2a1a6c43fdc7c0c2ed616241fb0ff69
Cordova-Rafael <rafaelcorsa97@gmail.com>
2023-05-24 22:41:52 -0500

    Gestionar clientes

--- 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Test.java
+++ 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Test.java
@@ -1,11 +1,16 @@
 package org.malibu.inventario.mapeo;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        Cliente p = new Cliente(123,"pepe");
-        System.out.println(p.getDni()+"\t"+p.getNombre());
+        Cliente obj = new Cliente();
+        List<Cliente> lista = new LinkedList<Cliente>();
+        obj.runCliente(lista);
 
                                             }
 }
