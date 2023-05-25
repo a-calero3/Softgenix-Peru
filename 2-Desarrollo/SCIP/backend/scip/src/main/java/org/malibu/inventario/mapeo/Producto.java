73cca6d22a22f8e0ecc20a6a2a17defa9abb676e
Cordova-Rafael <rafaelcorsa97@gmail.com>
2023-05-25 17:16:52 -0500

    Encapsulacion e implementacion de la clase Producto

--- 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Producto.java
+++ 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Producto.java
@@ -1,6 +1,61 @@
 package org.malibu.inventario.mapeo;
 
+import java.math.BigDecimal;
+
 public class Producto {
 
-    
-                      }
+    private int numero;
+    private String descripcion;
+    private BigDecimal precio;
+    private String fotos;
+    private String observaciones;
+
+    public Producto() {  }
+
+    public Producto(int numero, String descripcion, BigDecimal precio, String fotos, String observaciones) {
+        this.numero = numero;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.fotos = fotos;
+        this.observaciones = observaciones;                                                                 }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public String getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(String fotos) {
+        this.fotos = fotos;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+}
