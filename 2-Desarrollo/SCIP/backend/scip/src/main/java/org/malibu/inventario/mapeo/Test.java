54188a4d5da32538a697805273a3b45ba590aa67
Cordova-Rafael <rafaelcorsa97@gmail.com>
2023-05-24 21:03:51 -0500

    MapeoClase-Cliente

---
+++ 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Test.java
@@ -0,0 +1,11 @@
+package org.malibu.inventario.mapeo;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Cliente p = new Cliente(123,"pepe");
+        System.out.println(p.getDni()+"\t"+p.getNombre());
+
+                                            }
+}
