68f0c18f215bcb61c4fb5ae4a6c8ea155d0b3ad5
Cordova-Rafael <rafaelcorsa97@gmail.com>
2023-05-25 17:24:17 -0500

    Implementacion y encapsulacion de atributos CATEGORIA

--- 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Categoria.java
+++ 2-Desarrollo/SCIP/backend/scip/src/main/java/org/malibu/inventario/mapeo/Categoria.java
@@ -2,5 +2,28 @@ package org.malibu.inventario.mapeo;
 
 public class Categoria {
 
-    
-                        }
+    private String oid;
+    private String descripcion;
+
+    public Categoria() { }
+
+    public Categoria(String oid, String descripcion) {
+        this.oid = oid;
+        this.descripcion = descripcion;             }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
